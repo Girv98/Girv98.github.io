5dc0a06a028bd6b07bbd08c4022195936787d163
James Girven <32684786+Girv98@users.noreply.github.com>
2020-12-06 09:55:18 +0000

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+(function() {
+    var burger = document.querySelector('.burger');
+    var menu = document.querySelector('#'+burger.dataset.target);
+    var items = document.querySelectorAll('.navbar-item');
+    
+    burger.addEventListener('click', function() {
+        burger.classList.toggle('is-active');
+        menu.classList.toggle('is-active');
+	  
+	  items.forEach(function(item) {
+		  
+		  item.classList.toggle('is-white');
+	  });
+	  
+	  
+    });
+})();
---
+++ js/scroll.js
@@ -0,0 +1,6 @@
+  ScrollReveal().reveal('.has-scroll', {
+    delay: 400,
+    distance: '200px',
+    duration: 1500,
+    origin: 'bottom',
+});
