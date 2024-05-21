bfa81f3958652ed5215f5de2ad885ee9afc6e151
Girv98 <james.girven10@gmail.com>
2024-05-21 15:24:27 +0100

    Fixed pop-in on load

--- js/scroll.js
+++ js/scroll.js
@@ -1,6 +1,13 @@
-  ScrollReveal().reveal('.has-scroll', {
-    delay: 100,
-    distance: '200px',
-    duration: 1000,
-    origin: 'bottom',
+
+var items = document.querySelectorAll('.has-scroll');
+
+items.forEach((item) => {
+  item.style.visibility = 'visible';
+})
+  
+ScrollReveal().reveal('.has-scroll', {
+  delay: 100,
+  distance: '200px',
+  duration: 1000,
+  origin: 'bottom',
 });
